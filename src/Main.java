894383c5ba5d95339ce9e5478d8bdab8cb7da2c0
msedkiewicz <magdalenainspirations@gmail.com>
2026-01-29 19:50:10 +0100

    Add simple scanner

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main() {
+        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name\n");
+        String username = scanner.nextLine();
+        System.out.println("Username: " + username);
+    }
+}
