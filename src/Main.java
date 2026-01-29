5c623351aa36683ea61b41bfd77bb5657e471029
msedkiewicz <magdalenainspirations@gmail.com>
2026-01-29 21:27:59 +0100

    start adding rules logic

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main() {
-        RandomWord word = new RandomWord();
-        System.out.println(word.getWord());
+        System.out.println("Welcome to Console Hangman game!");
+        System.out.println("What is our name?");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name\n");
         String username = scanner.nextLine();
-        System.out.println("Username: " + username);
-        Hangman hangman = new Hangman();
-        System.out.println(hangman.getHangman());
+        System.out.println("Now, " + username + ", do you know rules of Hangman?");
+        System.out.println("Write: 0 for NO, 1 for YES");
+        String knowRules = scanner.nextLine();
+
+        int knowRulesYesNo = Integer.parseInt(knowRules);
+        if (knowRulesYesNo == 0) {
+            System.out.println("Hangman is a word-guessing game. Our board computer picks a random word.\n" +
+                    "Your task is to guess it within 11 attempts. With each attempt, you provide a letter.\n" +
+                    "For each incorrect guess, a part of a stick figure is drawn on the gallows.\n" +
+                    "For each correct guess, the letter appears in its place within the word.\n" +
+                    "The game ends when either the word is completely guessed or the hangman drawing is completed\n" +
+                    "(resulting in a loss).");
+            System.out.println("Are rules clear?");
+            System.out.println("Write: 0 for NO, 1 for YES");
+            String understandable = scanner.nextLine();
+            int understood = Integer.parseInt(knowRules);
+            if (understood == 0) {
+                System.out.println("Check this resource: https://en.wikipedia.org/wiki/Hangman_(game)\n" +
+                        "for a better understanding and come back, when you're ready! :)");
+            }
+        } else if (knowRulesYesNo == 1) {
+            System.out.println("Perfect! Time to play!");
+        } else {
+            System.out.println("");
+        }
+
+        RandomWord word = new RandomWord();
+/*        Hangman hangman = new Hangman();
+        System.out.println(hangman.getHangman());*/
     }
 }
