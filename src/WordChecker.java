8d87a918dd6414c890686d0248becadabae26bde
msedkiewicz <magdalenainspirations@gmail.com>
2026-01-31 11:31:07 +0100

    Add WordChecker class

---
+++ src/WordChecker.java
@@ -0,0 +1,40 @@
+public class WordChecker {
+    private String underscoredWord;
+    private String[] charactersUsed;
+
+    public String getUnderscoredWord() {
+        return underscoredWord;
+    }
+
+    public String setUnderscoredWord(String wordChecked) {
+        int wordLength = wordChecked.length();
+        for (int i = 0; i < wordLength; i++) {
+            underscoredWord = underscoredWord + "_ ";
+        }
+        return underscoredWord;
+    }
+
+    public String getCharactersUsed() {
+        return String.join(" ", charactersUsed);
+    }
+
+    public String[] addCharactersUsed(String character, int iteration) {
+        charactersUsed[iteration - 1] = character;
+        return charactersUsed;
+    }
+
+    public String wordShowedToUser(String character, String wordChecked) {
+        int wordLength = wordChecked.length();
+
+        String[] manipulatedWord = underscoredWord.split(" ");
+        String[] wordBeingChecked = wordChecked.split("");
+
+        for (int i = 0; i < wordLength; i++) {
+            if (wordBeingChecked[i].equals(character)) {
+                manipulatedWord[i] = character;
+            }
+        }
+
+        return String.join(" ", manipulatedWord);
+    }
+}
